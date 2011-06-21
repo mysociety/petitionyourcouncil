e04422c0717a542e66fd5dcdedded8185b066fe7
Edmund von der Burg <evdb@mysociety.org>
2011-06-21 12:24:03 +0100

    Resize map correctly

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -5,42 +5,45 @@ function initialize_map_cb() {
     var background_map = $("#background_map");
     var full_window    = $(window)
 
-    background_map.css({
-        height: full_window.height() + 'px',
-        width:  full_window.width()  + 'px',
-        position: 'fixed'
-    })
+    var map = false;
+    
+    var resize_background_map = function () {
+        // change the background div to be the size of the window
+        background_map.css({
+            height: full_window.height() + 'px',
+            width:  full_window.width()  + 'px',
+        })
+        // tell the map we've changed size (if it is set)
+        if (map)
+          google.maps.event.trigger(map, 'resize');
+    }
 
-  var myLatlng = new google.maps.LatLng( 54, -4 );
+    // set the resize cb and trigger it
+    full_window
+      .resize( resize_background_map )
+      .trigger("resize");
 
-  var map = new google.maps.Map(
-      document.getElementById("background_map"),
-      {
-          zoom: 6,
-          center: myLatlng,
 
-          // use the roadmap as it is probably clearest
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-
-          // try to make the map as static as possible - it is not meant to
-          // become a distraction
-          disableDefaultUI:       true,
-          disableDoubleClickZoom: true,
-          draggable:              false,
-          keyboardShortcuts:      false,
-          scrollwheel:            false
-      }
-  );
-
-  // var resize_background_map = function () {
-  // 
-  //     // FIXME - trigger map to resize
-  // 
-  // }
-  
-  // $(window)
-  //   .resize( resize_background_map )
-  //   .trigger("resize");
+    var myLatlng = new google.maps.LatLng( 54, -4 );
+      
+    map = new google.maps.Map(
+        document.getElementById("background_map"),
+        {
+            zoom: 6,
+            center: myLatlng,
+        
+            // use the roadmap as it is probably clearest
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+        
+            // try to make the map as static as possible - it is not meant to
+            // become a distraction
+            disableDefaultUI:       true,
+            disableDoubleClickZoom: true,
+            draggable:              false,
+            keyboardShortcuts:      false,
+            scrollwheel:            false
+        }
+    );
   
 }
 
