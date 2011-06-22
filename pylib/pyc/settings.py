1019f86b869262153e7a9eb05472df449f318fc2
Edmund von der Burg <evdb@mysociety.org>
2011-06-22 10:05:44 +0100

    Comment out messages - they are a Django 1.2 feature that we con't use on our 1.1 servers (and don't need anyway atm)

--- pylib/pyc/settings.py
+++ pylib/pyc/settings.py
@@ -109,7 +109,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    # 'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 ROOT_URLCONF = 'pyc.urls'
@@ -138,7 +138,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.messages',
+    'django.contrib.# messages',
 
     'django.contrib.admin',
 
