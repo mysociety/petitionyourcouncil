9635a8d954693bfea59451da2f3359b7b824128c
Edmund von der Burg <evdb@mysociety.org>
2011-06-29 16:22:37 +0100

    Faster searching by just using Google searches Don't need contact email anymore

--- pylib/pyc/core/forms.py
+++ pylib/pyc/core/forms.py
@@ -6,16 +6,19 @@ from core.models import Council
 
 class CouncilPetitonInfoForm(forms.ModelForm):
 
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        url   = cleaned_data.get("petition_url")
-        email = cleaned_data.get("contact_email")
-        
-        if not( url or email ):
-            raise forms.ValidationError("Please find a contact email if the petition_url is missing.")
-        
-        return cleaned_data
+    # def clean(self):
+    #     cleaned_data = self.cleaned_data
+    #     url   = cleaned_data.get("petition_url")
+    #     email = cleaned_data.get("contact_email")
+    #     
+    #     if not( url or email ):
+    #         raise forms.ValidationError("Please find a contact email if the petition_url is missing.")
+    #     
+    #     return cleaned_data
     
     class Meta:
         model = Council
-        fields = ( 'petition_url', 'petition_rss', 'contact_email' )
+        fields = (
+            'petition_url', 'petition_rss',
+             # 'contact_email'
+        )
